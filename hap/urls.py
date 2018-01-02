63397ef4f51e6bbd996b0fa8d1a06c234f69c3e1
wnse <yangkai07@gmail.com>
2018-01-03 00:54:05 +0800

    add css

--- hap/urls.py
+++ hap/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.index),
-	path('example', views.example)
+	path('home', views.home),
+	path('phase', views.phase),
+	path('about', views.about),
+	path('example',views.example),
 ]
+
--- hap/views.py
+++ hap/views.py
@@ -2,20 +2,28 @@ from django.shortcuts import render
 from .bio_src import phaseMethod
 # Create your views here.
 
-def index(request):
+def phase(request):
 	final_out=""
 	get = {}
 	phaseM = ""
 	if request.POST:
 		get['output'] = request.POST['input'] 
-		phaseM = request.POST['phase_method']
-		if phaseM == "method1":
-			final_out = phaseMethod.jabrehoo(get['output'])
-		else:
-			final_out = phaseMethod.method2(get['output'])
-	return render(request, 'index.html', {'output':final_out,'method':phaseM})
+	#	phaseM = request.POST['phase_method']
+	#	if phaseM == "method1":
+		final_out = phaseMethod.jabrehoo(get['output'])
+	#	else:
+	#		final_out = phaseMethod.method2(get['output'])
+	#return render(request, 'phase.html', {'output':final_out,'method':phaseM})
+	return render(request, 'phase.html', {'output':final_out})
 	
+def about(request):
+	return render(request, 'about.html')
+
+def home(request):
+#	return render(request,"introduction.html")
+	return render(request,"index.html")
+
 def example(request):
-	return render(request, 'example.html')
+	return render(request,'example.html')
 
 
